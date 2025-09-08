2f741d905eb38e28e989bc6c6ca9c69f53349975
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-09-08 11:52:08 +0700

    hotfix

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,44 +1,71 @@
-// app/editor/page.tsx
 "use client";
-import { useEffect, useRef } from "react";
-import grapesjs, { Editor } from "grapesjs";
-import "grapesjs/dist/css/grapes.min.css";
+import createStudioEditor from '@grapesjs/studio-sdk';
+import { tableComponent, listPagesComponent, lightGalleryComponent, iconifyComponent, accordionComponent, flexComponent, canvasEmptyState, canvasFullSize, canvasGridMode, layoutSidebarButtons } from '@grapesjs/studio-sdk-plugins';
+import '@grapesjs/studio-sdk/style';
 import { saveTemplate } from "@/lib/api";
 
 export default function EditorPage() {
-  // ref sẽ lưu instance của GrapesJS editor
-  const editorRef = useRef<Editor | null>(null);
-
-  useEffect(() => {
-    if (!editorRef.current) {
-      const editor = grapesjs.init({
-        container: "#gjs",
-        height: "100vh",
-        storageManager: false,
-      });
-      editorRef.current = editor;
-    }
-  }, []);
-
-  const handleSave = () => {
-    const editor = editorRef.current;
-    if (!editor) return;
-
-    const html = editor.getHtml();
-    const css = editor.getCss();
-    const json = editor.getComponents();
-
-    console.log("HTML:", html);
-    console.log("CSS:", css);
-    console.log("JSON:", json);
-
-    saveTemplate({ html, css, json })
-  };
-
-  return (
-    <div>
-      <button onClick={handleSave}>Save</button>
-      <div id="gjs" />
-    </div>
-  );
+// ...once the '#studio-editor' DOM element is rendered
+  createStudioEditor({
+    root: '#studio-editor',
+    licenseKey: '149bc6f4bc764a17ab6e08faeb5f40a3518121f29ac5476388011a9143a751da',
+    project: {
+      type: 'web'
+    },
+    assets: {
+      storageType: 'self',
+      // Provide a custom upload handler for assets
+      onUpload: async ({ files }) => {
+        const body = new FormData();
+        for (const file of files) {
+          body.append('files', file);
+        }
+        const response = await fetch('ASSETS_UPLOAD_URL', { method: 'POST', body });
+        const result = await response.json();
+        // The expected result should be an array of assets, eg.
+        // [{ src: 'ASSET_URL' }]
+        return result;
+      },
+      // Provide a custom handler for deleting assets
+      onDelete: async ({ assets }) => {
+        const body = JSON.stringify(assets);
+        // await fetch('ASSETS_DELETE_URL', { method: 'DELETE', body });
+        console.log('Delete these assets:', assets);
+      }
+    },
+    storage: {
+      type: 'self',
+      // Provide a custom handler for saving the project data.
+      onSave: async ({ project }) => {
+        throw new Error('Implement your "onSave"!');
+        const body = new FormData();
+        body.append('project', JSON.stringify(project));
+        // await fetch('PROJECT_SAVE_URL', { method: 'POST', body });
+        await saveTemplate(project);
+        console.log('Project data to be saved:', project);
+      },
+      // Provide a custom handler for loading project data.
+      onLoad: async () => {
+        throw new Error('Implement your "onLoad"!');
+        // const response = await fetch('PROJECT_LOAD_URL');
+        // const project = await response.json();
+        // // The project JSON is expected to be returned inside an object.
+        // return { project };
+      },
+      autosaveChanges: 100,
+      autosaveIntervalMs: 10000
+    },
+    plugins: [
+      tableComponent.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/components/table */ }),
+      listPagesComponent.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/components/listPages */ }),
+      lightGalleryComponent.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/components/lightGallery */ }),
+      iconifyComponent.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/components/iconify */ }),
+      accordionComponent.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/components/accordion */ }),
+      flexComponent.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/components/flex */ }),
+      canvasEmptyState.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/canvas/emptyState */ }),
+      canvasFullSize.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/canvas/full-size */ }),
+      canvasGridMode.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/canvas/grid-mode */ }),
+      layoutSidebarButtons.init({ /* Plugin options: https://app.grapesjs.com/docs-sdk/plugins/layout/sidebar-buttons */ })
+    ]
+  });
 }
